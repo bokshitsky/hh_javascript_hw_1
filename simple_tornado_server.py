e3c8797d27991bbcab41c045e13418f356d75e24
Boksh <bokshitsky@gmail.com>
2016-04-03 22:12:37 +0300

    Now works only usin web sockets

---
+++ simple_tornado_server.py
@@ -0,0 +1,86 @@
+import tornado.ioloop
+import tornado.web
+import tornado.websocket
+
+from tornado.options import define, options, parse_command_line
+from json import loads, dumps
+
+define("port", default=8888, help="run on the given port", type=int)
+
+
+
+class IndexHandler(tornado.web.RequestHandler):
+    @tornado.web.asynchronous
+    def get(self):
+        self.render("static/list.html")
+
+
+items = []
+listeners = set()
+class ItemsHandler(tornado.web.RequestHandler):
+    @tornado.web.asynchronous
+    def get(self):
+        self.write({"data":items})
+        self.finish()
+
+
+def informAllListenersExcept(message, exception):
+    for listener in listeners:
+        if listener != exception:
+            listener.ws_connection.write_message(message)
+
+def informAboutUpdate(exception):
+    data = {"method":"update", "elements":items}
+    informAllListenersExcept(dumps(data), exception)
+
+
+ActionsMap = {}
+def swap(data):
+    index1 = data["old_position"]
+    index2 = data["new_position"]
+    item = items[index1]
+    items.remove(item)
+    items.insert(index2,item)
+
+ActionsMap["add"] = lambda data: items.append(data["item"])
+ActionsMap["swap"] = swap
+
+                                           
+class UpdateInformer(tornado.websocket.WebSocketHandler):
+    def open(self):
+        listeners.add(self)
+        data = {"method":"update", "elements":items}
+        self.ws_connection.write_message(dumps(data))
+        print("connection started")
+
+
+    def on_message(self, message):
+        data = loads(message)
+        action = ActionsMap[data["method"]]
+        action(data)
+        informAboutUpdate(exception=self)
+        
+
+    def on_close(self, message=None):
+        listeners.remove(self)
+        print("connection closed")
+
+
+
+class MyStaticFileHandler(tornado.web.StaticFileHandler):
+    def set_extra_headers(self, path):
+        self.set_header('Cache-Control', 'no-store, no-cache, must-revalidate, max-age=0')
+
+    
+
+app = tornado.web.Application([
+    (r'/', IndexHandler),
+    (r'/items', ItemsHandler),
+    (r'/updateinformer', UpdateInformer),
+    (r"/static/(.*)", MyStaticFileHandler, {"path": "static"})
+])
+
+if __name__ == '__main__':
+    parse_command_line()
+    app.listen(options.port)
+    tornado.ioloop.IOLoop.instance().start()
---
+++ static/main.js
@@ -0,0 +1,115 @@
+"use strict";
+
+
+
+(function (){
+    
+    function arrayMove(arr, fromIndex, toIndex) {
+        var element = arr[fromIndex];
+        arr.splice(fromIndex, 1);
+        arr.splice(toIndex, 0, element);
+    }
+    
+    function getDomElementPosition (parent, element) {
+        var Elements = parent.children;
+        for (var i = 0; i < Elements.length; i++) {
+            if (Elements[i] == element) {
+                return i;
+            }
+        }
+        return -1
+    }
+
+    
+    var list = document.getElementById("list");
+    var inputField = document.getElementById("dataInputField");
+    var elements = [];
+    var i = 1;
+    
+    
+    function rebuildItemsList(){
+        list.innerHTML = "";
+        for (var i = 0; i < elements.length; i++) {
+            var newNode = document.createElement("li");
+            newNode.setAttribute("draggable",true);
+            newNode.setAttribute("data-element-id", elements[i].id );
+            newNode.appendChild(document.createTextNode(elements[i].text));
+            list.appendChild(newNode);
+        }
+    }
+    
+    var ws = new WebSocket("ws://localhost:8888/updateinformer");
+    var actionsMap = {};
+    actionsMap["update"] = function(data){
+        elements = data["elements"]
+        i = elements.length + 1;
+        rebuildItemsList()
+    }
+    
+    ws.onmessage = function (evt) {
+        var data = JSON.parse(evt.data);
+        actionsMap[data.method](data); //ACTUALLY WE HAVE ONLY ONE METHOD.
+    };
+    
+    function swapTwoItems(index1, index2) {
+        var body = JSON.stringify({method:"swap", old_position:index1, new_position:index2})
+        ws.send(body);
+        arrayMove(elements,index1,index2);
+    }
+    
+    function createNewItem() {
+        var newElement = {id:i, text:inputField.value};
+        var body = JSON.stringify({method:"add", item:newElement});
+        ws.send(body);
+        var newNode = document.createElement("li");
+        newNode.setAttribute("draggable",true);
+        newNode.setAttribute("data-element-id", i);
+        
+        elements.push(newElement);
+        newNode.appendChild(document.createTextNode(newElement.text));
+        list.appendChild(newNode);
+    }
+    
+    
+    inputField.addEventListener(
+        "keypress", 
+        function(e){
+            var inputField = e.target; //we can either use dataInput variable;
+            if (e.keyCode === 13 && inputField.value != "") {
+                e.preventDefault();
+                
+                createNewItem()
+                inputField.value = "";
+                i++; 
+            }
+        })
+    
+    
+    list.ondragstart = function(e){
+                    var dragListItem = e.target;
+                    var startPosition = getDomElementPosition(list, dragListItem)
+                    var endPosition = -1;
+        
+                    dragListItem.style.opacity = '0.3';
+                    e.dataTransfer.setData('text/plain', "");
+                    
+                    list.ondragenter= function(e){
+                        var curPos = getDomElementPosition(list, dragListItem);
+                        var newPos = getDomElementPosition(list, e.target);
+                        
+                        if (newPos >= 0) {
+                            endPosition = newPos
+                            if (newPos > curPos) {
+                                list.insertBefore(dragListItem, e.target.nextSibling);
+                            } else if (newPos < curPos) {
+                                list.insertBefore(dragListItem, e.target);    
+                            }
+                        }
+                    }
+                    
+                    list.ondragend= function(e) {
+                        dragListItem.style.opacity = '';
+                        if (endPosition != -1) {swapTwoItems(startPosition, endPosition);}
+                    }
+                }
+})();
